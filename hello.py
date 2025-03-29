752eda080f81d772fcd94b551225518bb1303447
Ralbe <90103043+Ralbe@users.noreply.github.com>
2025-03-29 22:55:27 +0300

    Настройка страниц

---
+++ hello.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def main():
+    return render_template('main.html')
+
+
+@app.route('/registor')
+def registor():
+    return render_template('registor.html')
+
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # показать профиль данного пользователя
+    return 'User %s' % username
+
+
+if __name__ == '__main__':
+    app.run(debug = True)
